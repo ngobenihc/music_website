56f1751e96af566f8465988344202b583cd3fc9e
ngobenihc <ngobenihc@yahoo.com>
2023-09-18 13:52:14 +0200

    music player

---
+++ javaScript/music_details.js
@@ -0,0 +1,38 @@
+ let allMusic = [
+  {
+    name: "Yes and Amen",
+    artist: "Jesus Culture",
+    img: "images/yes_and_amen.jpeg",
+    src: "songs/Yes_And_Amen.mp3",
+  },
+  {
+    name: "Praise You In This Storm",
+    artist: "Casting Crowns",
+    img: "images/casting-crowns.jpg",
+    src: "songs/Praise_You_In_This_Storm.mp3",
+  },
+  {
+    name: "Give Me Faith",
+    artist: "Elevation Worship",
+    img: "images/ElevationWorship.jpg",
+    src: "songs/Give_Me_Faith.mp3",
+  },
+  {
+    name: "I Can Only Imagine",
+    artist: "Original_Movie_Soundtrack",
+    img: "images/I-Can-Only-Imagine.jpg",
+    src: "songs/I_Can_Only_Imagine.mp3",
+  },
+  {
+    name:"You say",
+    artist:"Lauren Daigle",
+    img:"images/you say.jpg",
+    src:"songs/You_Say.mp3"
+  },
+  {
+    name:"Unstoppable God",
+    artist:"Elevation Worship",
+    img:"images/ws.png",
+    src:"songs/Unstoppable_God.mp3"
+  }
+];
